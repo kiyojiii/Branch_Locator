439ddefc1fd1b4b231928389ec364855ea9fa584
Kiyoji <clintjoshua.velasquez@g.msuiit.edu.ph>
2023-10-08 22:47:06 +0800

    First Commit

---
+++ blocator.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# localhost:5000/user/name
+@app.route('/user/<name>')
+def user(name):
+    return "Hello {}".format(name)
+
+    
